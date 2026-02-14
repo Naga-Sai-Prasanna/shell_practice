e0259d8e18c9c4c8b401dbc8f8d118dcfacabf7e
PRASANNA <prasanna@PRASANNAs-MacBook-Air.local>
2026-02-14 13:42:24 +0530

    variable

--- 09-variable.sh
+++ 09-variable.sh
@@ -1,23 +1,5 @@
 #!/bin/bash
-PERSON1=ABHI
-PERSON2=SASHI
-
-echo "$PERSON1::HI $PERSON2"
-echo "$PERSON2::Bye $PERSON1"
-
-USER1=$1
-USER2=$2
-
-echo "$USER1::HI $$USER2"
-echo "$USER2::Bye $USER1"
-
-echo "args paased :: $@"
-echo "pwd :: $PWD"
-echo "username :: $USER"
-echo "script nzme :: $0"
-echo "home :: $HOME"
-echo "pid of the script:$$"
-sleep 10 &
-echo "recent process ::$!"
-echo " no.of var:: $#"
-
+num1=100
+num2=20
+sum=$(($num1+$num2))
+echo "sum is:$sum"
